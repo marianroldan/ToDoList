e0ed21dcd491d3367c5da25710bd69f0912107b6
marianroldan <77456919+marianroldan@users.noreply.github.com>
2021-04-26 11:27:46 -0500

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,19 @@
+document.getElementById("addBtn").addEventListener("click", addText);
 
+function addText(){
+  var listItem = document.createElement("li");
+  listItem.innerText = document.getElementById("myInput").value;
+  document.getElementById("myUl").appendChild(listItem);
+ 
+  var deleteButton = document.createElement('button');
+  deleteButton.className = "delete";
+  deleteButton.onclick=removeText;
+  deleteButton.innerText = "x";
+  listItem.appendChild(deleteButton);
+}
+
+function removeText(){
+  var listItem = this.parentNode;
+  var ul=listItem.parentNode;
+  ul.removeChild(listItem);
+}                                   
